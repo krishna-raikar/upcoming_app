126fc70f314ffbe0bc8485d6307654830c737b68
krishna <krishna.raikar@kvpcorp.com>
2018-04-25 23:27:07 +0530

    basic flow ready

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # function called after user signing in.
+  # Redirect to organization creation page if one not created else to the welcome page.
+  def after_sign_in_path_for(user)
+    user.organization.nil? ? new_organization_path : root_path
+  end
+
 end
